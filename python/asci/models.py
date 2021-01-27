c7dbd9878136b4d00cc9c7678c54d26fe027fc5b
François Rozet <francois.rozet@outlook.com>
2021-01-27 17:57:03 +0100

    🚧 Models
    * Fork Classifier (FC)

--- python/asci/models.py
+++ python/asci/models.py
@@ -3,7 +3,7 @@
 import torch
 import torch.nn as nn
 
-from typing import Callable
+from typing import Callable, List
 
 
 class MLP(nn.Sequential):
@@ -27,18 +27,63 @@ class MLP(nn.Sequential):
         hidden_size: int = 64,
         bias: bool = True,
         dropout: float = 0.,
-        activation: Callable[[], nn.Module] = nn.PReLU,
+        activation: Callable[[int], nn.Module] = nn.PReLU,
     ):
         if dropout > 0.:
             dropout = nn.Dropout(dropout)
         else:
             dropout = nn.Identity()
 
-        layers = [nn.Linear(input_size, hidden_size, bias), activation(), dropout]
+        layers = [nn.Linear(input_size, hidden_size, bias), activation(hidden_size), dropout]
 
         for i in range(num_layers):
-            layers.extend([nn.Linear(hidden_size, hidden_size, bias), activation(), dropout])
+            layers.extend([nn.Linear(hidden_size, hidden_size, bias), activation(hidden_size), dropout])
 
         layers.append(nn.Linear(hidden_size, output_size, bias))
 
         super().__init__(*layers)
+
+        self.input_size = input_size
+        self.output_size = output_size
+
+
+class ForkClassifier(nn.Module):
+    r"""Fork Classifier (FC)
+
+                    ---> f_a(y_a, z(x))
+                   /
+    x ---> z(x), y ----> f_b(y_b, z(x))
+                   \
+                    ---> f_c(y_c, z(x))
+
+    Args:
+        encoder: A forward module to process `x`.
+        masks: The masks of each considered subset of `y`.
+
+        **kwargs are transmitted to `MLP`.
+    """
+
+    def __init__(self, encoder: nn.Module, masks: torch.BoolTensor, **kwargs) -> torch.Tensor:
+        super().__init__()
+
+        self.encoder = encoder
+
+        self.register_buffer('masks', masks)
+
+        self.heads = nn.ModuleList([
+            MLP(input_size=int(size) + self.encoder.output_size, **kwargs)
+            for size in self.masks.sum(dim=-1)
+        ])
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor, select: List[int] = None) -> torch.Tensor:
+        z = self.encoder(x)
+
+        if select is None:
+            select = range(len(self.heads))
+
+        fs = []
+        for i, (mask, head) in enumerate(zip(self.masks, self.heads)):
+            if i in select:
+                fs.append(head(torch.cat([y[..., mask], z], dim=-1)))
+
+        return torch.cat(fs, dim=-1)
