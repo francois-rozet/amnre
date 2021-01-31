9ee226bc69ab4ef125fe124cd21cd9714d1d6a08
François Rozet <francois.rozet@outlook.com>
2021-01-31 20:57:19 +0100

    🚧 Datasets
    * Simplify LTE and MLTE
    * Remove CLTE

--- python/datasets.py
+++ python/datasets.py
@@ -9,80 +9,36 @@ from typing import Tuple
 from simulators import Simulator
 
 
-class LTEDataset(data.Dataset):
+class LTEDataset(data.IterableDataset):
     r"""Likelihood-To-Evidence Dataset"""
 
-    def __init__(self, sim: Simulator):
+    def __init__(
+        self,
+        simulator: Simulator,
+        masks: torch.BoolTensor = None,
+        batch_size: int = 256,
+    ):
         super().__init__()
 
-        self.sim = sim
-        self.theta, self.x = [], []
+        self.simulator = simulator
+        self.batch_size = batch_size
 
-        self._true = torch.ones(self.sim.theta_size, dtype=bool)
-        self._false = ~self._true
-
-    def save(self, f: str) -> None:
-        torch.save((self.theta, self.x), f)
-
-    def load(self, f: str):  # -> self
-        self.theta, self.x = torch.load(f)
-        return self
-
-    def simulate(self, size: int, batch: int = 1024):  # -> self
-        theta, x = [], []
-
-        for s in [batch] * (size // batch) + [size % batch]:
-            samples = self.sim.sample((s,))
-
-            theta.append(samples[0].cpu())
-            x.append(samples[1].cpu())
-
-        self.theta, self.x = torch.cat(theta), torch.cat(x)
-        return self
-
-    def __len__(self) -> int:
-        return len(self.theta)
-
-    def __getitem__(self, i: int) -> Tuple[torch.Tensor, torch.Tensor, torch.BoolTensor]:
-        theta, x = self.theta[i], self.x[i]
-
-        mask = self.random_mask()
-        theta = mask * theta + ~mask * self.sim.prior.sample()
-
-        return theta, x, mask
-
-    def random_mask(self) -> torch.BoolTensor:
-        if random.random() < 0.5:
-            return self._true
+        if masks is None:
+            self.masks = torch.ones_like(
+                self.simulator.prior.sample()
+            ).bool().unsqueeze(0)
         else:
-            return self._false
+            self.masks = masks
 
+    def __iter__(self) -> Tuple[torch.Tensor, torch.Tensor, torch.BoolTensor]:
+        while True:
+            theta, x = self.simulator.sample((self.batch_size,))
+            theta_prime = self.simulator.prior.sample((self.batch_size,))
 
-class CLTEDataset(LTEDataset):
-    r"""Conditional Likelihood-To-Evidence Dataset"""
+            mask = random.choice(self.masks)
 
-    def __init__(self, masks: torch.BoolTensor, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+            # (theta_a, theta_b', x)
+            yield mask * theta + ~mask * theta_prime, x, mask
 
-        self.masks = masks
-
-    def random_mask(self) -> torch.Tensor:
-        if random.random() < 0.5:
-            return self._true
-        else:
-            return ~random.choice(self.masks)
-
-
-class MLTEDataset(LTEDataset):
-    r"""Marginal Likelihood-To-Evidence Dataset"""
-
-    def __init__(self, masks: torch.BoolTensor, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        self.masks = masks
-
-    def random_mask(self) -> torch.Tensor:
-        if random.random() < 0.5:
-            return random.choice(self.masks)
-        else:
-            return self._false
+            # (theta_a', theta_b, x)
+            yield ~mask * theta + mask * theta_prime, x, ~mask
