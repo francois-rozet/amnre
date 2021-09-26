a3f9b481b431c030fe8b778c0a95e0b45454938c
François Rozet <francois.rozet@outlook.com>
2021-09-26 18:02:59 +0200

    🔥 Replace stephengreen/lfi-gw by francois-rozet/lfi-gw

--- misc/gw_setup.sh
+++ misc/gw_setup.sh
@@ -7,10 +7,9 @@ pip install gwpy pycbc bilby
 CACHE=~/.cache/lfi-gw
 
 if ! [ -d $CACHE ]; then
-    git clone https://github.com/stephengreen/lfi-gw $CACHE
-    jupyter nbconvert --to notebook --execute $CACHE/notebooks/GW150914_data.ipynb --inplace --allow-errors
+    git clone https://github.com/francois-rozet/lfi-gw $CACHE
 fi
 
 cp -r $CACHE/lfigw amnre/simulators
 cp -r $CACHE/data/* amnre/simulators/lfigw
-cp $CACHE/bilby_runs/bilby_GW150914.py misc/references/bilby_train.py
+cp -r $CACHE/bilby_runs/* misc/references
